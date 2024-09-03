1ef89fb76fa9e4d954b1231286233823731cefa1
Yiroma <yiromaric@gmail.com>
2024-09-03 18:04:33 +0200

    start project and init pages and create navbar and navlink components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,38 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* IMAGE CONTAINER */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <Image src="/hero.png" alt="image d'accueil" fill className="object-contain" />
+      </div>
+      {/* TEXT CONTAINER */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+        {/* TITLE */}
+        <h1 className="text-4xl md:text-6xl font-bold">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </h1>
+
+        {/* DESC */}
+        <p className="md:text-xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid in fuga amet ipsum enim
+          impedit natus nulla eligendi rem earum accusantium facilis unde, maxime expedita! Nihil
+          eveniet fugiat, provident, fugit earum dolores quo adipisci nesciunt ad at vero
+          accusantium cum exercitationem deserunt architecto recusandae possimus nobis dolorem id
+          veniam ducimus!
+        </p>
+
+        {/* BUTTONS */}
+        <div className="w-full flex gap-4">
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+            View My Work
+          </button>
+          <button className="p-4 rounded-lg ring-1 ring-black">Contact Me</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
