5bfbed2d55ff7a2940c12d9ca5858654745c0a8e
Yiroma <yiromaric@gmail.com>
2024-09-06 14:55:50 +0200

    create Brain svg Component and animation it

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useScroll } from "framer-motion";
 import { BiographySign, ScrollIcon } from "@/components/SvgIcons";
+import Brain from "@/components/brain";
+import { useRef } from "react";
 
 const skills = [
   "JavaScript",
@@ -37,6 +39,10 @@ const experiences = {
 };
 
 const AboutPage = () => {
+  const containerRef = useRef();
+
+  const { scrollYProgress } = useScroll({ container: containerRef });
+
   return (
     <motion.div
       className="h-full"
@@ -45,7 +51,7 @@ const AboutPage = () => {
       transition={{ duration: 0.6 }}
     >
       {/* CONTAINER */}
-      <div className="h-full overflow-scroll lg:flex">
+      <div className="h-full overflow-scroll lg:flex" ref={containerRef}>
         {/* TEXT CONTAINER */}
         <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2">
           {/* BIOGRAPHY CONTAINER */}
@@ -198,7 +204,9 @@ const AboutPage = () => {
           </div>
         </div>
         {/* SVG CONTAINER */}
-        <div className="hidden lg:block w-1/3 xl:w-1/2">SVG</div>
+        <div className="hidden lg:block w-1/3 xl:w-1/2 sticky top-0 z-30">
+          <Brain scrollYProgress={scrollYProgress} />
+        </div>
       </div>
     </motion.div>
   );
